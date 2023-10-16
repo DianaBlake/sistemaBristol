50f5a5ad2e02cb860d52ea0c9ed66ebbe6566912
SrAlexjajaXD <alexloga18@gmail.com>
2023-10-15 18:22:43 -0600

    Se creo el diseño de la pagina orders (completa pedido), queda pendiente seccion de direcciones

--- src/pages/OrderPage.js
+++ src/pages/OrderPage.js
@@ -9,8 +9,26 @@ function OrderPage() {
     <>
       <Encabezado />
       <div>
-        <p className={estilo.Titulo}>Completa tu pedido</p>
-        <CarritoTabla/>
+        <p className={estilo.Titulo}>COMPLETA TU PEDIDO</p>
+        <div className={estilo.areaCompra}>
+          <div className={estilo.seccionBolsa}>
+            <CarritoTabla/>
+          </div>
+          <div className={estilo.seccionPago}>
+            <p className={estilo.tituloSecundario}>Método de pago</p>
+            <div className={estilo.seccionPagoInfo}>
+              <p className={estilo.textoNormalIZQ}>Subtotal: </p>
+              <p className={estilo.textoNormalDER}>SUB</p>
+              <p className={estilo.textoNormalIZQ}>Descuento: </p>
+              <p className={estilo.textoNormalDER}>DES</p>
+              <p className={estilo.textoNormalIZQ}>Cupones: </p>
+              <p className={estilo.textoNormalDER}>CUP</p>
+              <p className={estilo.textoNormalIZQ}>Envío: </p>
+              <p className={estilo.textoNormalDER}>ENV</p>
+            </div>
+            <button type="" className={estilo.botonPago}>REALIZAR COMPRA</button>
+          </div>
+        </div>
       </div>
     </>
   )
